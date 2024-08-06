ccaba2a83c6c8eab81c1827c401728f67c630bdc
joker <pcjoker10@gmail.com>
2024-08-06 22:38:30 +0900

    PM2 install

---
+++ syntax/function.js
@@ -0,0 +1,17 @@
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+f123();
+console.log('C');
+console.log('D');
+console.log('E');
+console.log('f');
+console.log('g');
+f123();
+
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
