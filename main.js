eaf6ca3219e15a081f810fff15809f2c58843e0c
joker <pcjoker10@gmail.com>
2024-08-18 18:38:18 +0900

    Nodejs Express install

--- main.js
+++ main.js
@@ -1,42 +1,58 @@
-var http = require('http');
-var url = require('url');
+var express = require('express');
+var app = express();
+var port = 3000;
+
 var topic = require('./lib/topic');
 var author = require('./lib/author');
 
-var app = http.createServer(function(request,response){
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-  
-  if(pathname === '/'){
-    if(queryData.id === undefined){
-      topic.home(request, response);
-    } else {
-      topic.page(request, response);
-    }
-  } else if(pathname === '/create') {
-    topic.create(request, response);
-  } else if(pathname === '/create_process') {
-    topic.create_process(request, response);
-  } else if(pathname === '/update') {
-    topic.update(request, response);
-  } else if(pathname === '/update_process') {
-    topic.update_process(request, response);
-  } else if (pathname === '/delete_process') {
-    topic.delete_process(request, response);
-  } else if (pathname === '/author') {
-    author.home(request, response);
-  } else if (pathname === '/author/create_process') {
-    author.create_process(request, response);
-  } else if (pathname === '/author/update') {
-    author.update(request, response);
-  } else if (pathname === '/author/update_process') {
-    author.update_process(request, response);
-  } else if (pathname === '/author/delete_process') {
-    author.delete_process(request, response);
-  } else {
-    response.writeHead(404);
-    response.end('Not found');
-  }
+app.get('/', (request, response) => {
+  topic.home(request, response);
 });
-app.listen(3000);
+
+app.get('/page/:pageId', (request, response) => {
+  topic.page(request, response);
+});
+
+app.get('/create', (request, response) => {
+  topic.create(request, response);
+});
+
+app.post('/create_process', (request, response) => {
+  topic.create_process(request, response);
+});
+
+app.get('/update/:pageId', (request, response) => {
+  topic.update(request, response);
+});
+
+app.post('/update_process', (request, response) => {
+  topic.update_process(request, response);
+});
+
+app.post('/delete_process', (request, response) => {
+  topic.delete_process(request, response);
+});
+
+app.get('/author', (request, response) => {
+  author.home(request, response);
+});
+
+app.post('/author/create_process', (request, response) => {
+  author.create_process(request, response);
+});
+
+app.get('/author/update', (request, response) => {
+  author.update(request, response);
+});
+
+app.post('/author/update_process', (request, response) => {
+  author.update_process(request, response);
+});
+
+app.post('/author/delete_process', (request, response) => {
+  author.delete_process(request, response);
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${port}`)
+});
